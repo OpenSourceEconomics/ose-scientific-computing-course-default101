57a3fbaec9f5d22479dbd7f8fc51dee6825eea99
alexandrosgi <34195760+alexandrosgi@users.noreply.github.com>
2021-12-06 16:11:10 +0100

    Added solution method now for real...

--- auxiliary/FirmInvestmentModel.py
+++ auxiliary/FirmInvestmentModel.py
@@ -97,3 +97,71 @@ class FirmInvestmentModel:
 
         # Static payoff is shareholder payoff
         self.Rmat = np.reshape(CF_share_mat, (self.statenum, self.nk))
+
+
+@jit(nopython=True)
+def solve_model_notreshaped(terry):
+    """
+    Solves the model.
+
+    Parameters
+    ----------
+    * terry:    instance of the class FirmInvestmentModel
+
+    Output (not reshaped yet!)
+    ------
+    * Vold (vector of dimension terry.statenum):        value function
+    * polind (vector of dimension terry.statenum):      index of optimal policy on the k grid
+    * pol (vector of dimension terry.statenum)          optimal policy (point on the k grid)
+    * equity_iss (vector of dimension terry.statenum)   negative part of equity issuance
+    """
+    
+    # Initialize the guess for the value function
+    Vold = np.zeros(terry.statenum)
+    polindold = np.zeros(terry.statenum)
+    # polold = polindold # commented out calc of pol during the loop
+
+    i = 0
+    solerr = terry.precision + 1
+    verbose=True
+    print_skip=25
+
+    while i < terry.max_iter and solerr > terry.precision:
+        
+        EVmat = np.kron((np.reshape(Vold, (terry.nz, terry.nk)).T@terry.pr_mat_m.T), np.ones((1, terry.nk))).T
+
+        RHSmat = terry.Rmat + terry.beta*EVmat
+        V, polind = np_max_axis1(RHSmat)
+        # pol = terry.kvec[polind]
+        
+        solerr = np.max(np.abs(V-Vold))
+        # polerr = np.max(np.abs(pol-polold))
+
+        i += 1
+        if verbose and i % print_skip == 0:
+            print(i , "(",  solerr,' error)')
+        
+        minshift = (terry.beta/(1-terry.beta))*np.min(V-Vold)
+        maxshift = (terry.beta/(1-terry.beta))*np.max(V-Vold)
+        Vold = V+(maxshift + minshift)/2
+        polinold = polind
+        # polold = pol
+        
+    if i == terry.max_iter:
+        print("Failed to converge!")
+
+    if verbose and i < terry.max_iter:
+        print("\nConverged in", i, " iterations.")
+
+    # optimal policy
+    pol = terry.kvec[polind]
+
+    # Select equity issuance based on optimised policy index
+    equity_iss = np.empty(len(polind))
+    for i in np.arange(0, terry.statenum):
+        equity_iss[i] = terry.CFfirm_mat[i, polind[i]]
+
+    equity_iss[equity_iss >= 0] = 0
+    equity_iss[equity_iss < 0] = -equity_iss[equity_iss<0]
+
+    return Vold, polind, pol, equity_iss
