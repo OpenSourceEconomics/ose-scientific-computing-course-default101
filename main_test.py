2ddbe548f7c03d2dea1bd2b9ffb484105ade2640
alexandrosgi <34195760+alexandrosgi@users.noreply.github.com>
2021-12-17 17:08:39 +0100

    Modified notebooks and main_test for testing purposes.

--- main_test.py
+++ main_test.py
@@ -10,6 +10,9 @@ if __name__=='__main__':
     
     seed = 10082021
     np.random.seed(seed)
+
+    alpha = 0.5
+    delta = 0.05
     
     deep_param = {
         "beta" : 0.96,
@@ -25,31 +28,31 @@ if __name__=='__main__':
 
     approx_param = {
         "max_iter" : 30, 
-        "precision" : 1e-4, 
+        "precision" : 1e-3, 
         "size_capital_grid" : 101, 
     }
 
     sim_param = {
-        "number_firms" : 10, 
+        "number_firms" : 2, 
         "number_simulations_per_firm" : 1, 
-        "number_years_per_firm" : 30, 
-        "burnin" : 200, 
+        "number_years_per_firm" : 10, 
+        "burnin" : 30, 
         "seed" : 10082021
     }
 
-    alpha = 0.5
-    delta = 0.05
+    visualization_param = {
+        "alpha grid bounds" : (0.35, 0.65), 
+        "delta grid bounds" : (0.01,0.1),
+        "fixed alpha" : alpha, 
+        "fixed delta" : delta, 
+        "parameter grid size" : 15
+    }
 
-    model = Model(deep_param, discretization_param)
-    model._solve_model(alpha, delta, approx_param)
+    model = Model(deep_param, discretization_param, approx_param)
+    # model._solve_model(alpha, delta, approx_param)
 
-    nz = 11         # Number of grid points for TFP
-    emean = 0.0     # Mean of innovations to firm TFP
-    sigma_z = 0.15  # Standard Deviations of innovations
-    rho = 0.7       # TFP persistence
-    multiple = 2.575 # Number of standard deviations the grid should span
-
-    print("half")
+    # model.visualize_model_sol(alpha, delta, approx_param)
+    model.visualize_mom_sensitivity(visualization_param, sim_param)
 
     # Productivity shock grid and transition probability matrix
     # mgrid, pr_mat_m = approx_markov(rho, sigma_z, multiple, nz)
